983d7f196be2bf209ea68081912fa5d9b2016adc
gaozhihan <gaozhihan@users.noreply.github.com>
2018-01-14 15:38:11 +0800

    debug

--- pre-trained/demos/temp_im2mask.m
+++ pre-trained/demos/temp_im2mask.m
@@ -8,18 +8,18 @@ file_list = dir(data_mat_path);
 num_data = (length(file_list) - 2) / 6; % do not count '.' and '..'
 
 for idx = 0:(num_data - 1)
-    load(fullfile(data_mat_path, sprintf('seq_%d.mat', num_data)), '-mat');
+    load(fullfile(data_mat_path, sprintf('seq_%d.mat', idx)), '-mat');
     seq = uint8(seq * 255);
     seq_size = size(seq);
     batch_size = seq_size(2);
     seq_len = seq_size(1);
-    rp_batch_dir = fullfile(rp_mat_path, sprintf('rp_%d', num_data));
+    rp_batch_dir = fullfile(rp_mat_path, sprintf('rp_%d', idx));
     mkdir(rp_batch_dir);
     for batch_idx = 0:(batch_size - 1)
         frame = seq(seq_len, batch_idx, 1, :, :);
         [candidates_mcg, ucm2_mcg] = im2mcg(frame, 'accurate', 1);
         region_proposal = candidates_mcg.masks;
-        rp_path = fullfile(rp_batch_dir, sprintf('rp_%d_idx_%d', num_data, batch_idx));
+        rp_path = fullfile(rp_batch_dir, sprintf('rp_%d_idx_%d', idx, batch_idx));
         save(rp_path, 'region_proposal')
     end
 end
