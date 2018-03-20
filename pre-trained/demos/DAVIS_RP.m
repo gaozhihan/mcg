079538ca9231ca6e86036d2830d99d04cf8b6a33
gaozhihan <gzh93@outlook.com>
2018-03-20 16:47:10 +0800

    script for generating rps on DAVIS dataset

---
+++ pre-trained/demos/DAVIS_RP.m
@@ -0,0 +1,35 @@
+function [ consumed_time ] = DAVIS_RP( idx_start, idx_end, gpu_idx )
+
+data_root_dir = '/home/data/gaozhihan/project/davis-2017/data/DAVIS/JPEGImages/480p';
+rp_root_dir = '/home/data/gaozhihan/project/davis-2017/data/DAVIS/RegionProposals/480p';
+data_list = dir(data_root_dir); 
+data_list = data_list(3: end); % the first two elements are "." and ".."
+
+gpuDevice(gpu_idx)
+for idx = idx_start: idx_end
+    disp(['generating rps for sequence ', data_list(idx).name]);
+    sequence_dir = fullfile(data_root_dir, data_list(idx).name);
+    rp_sequence_dir = fullfile(rp_root_dir, data_list(idx).name);
+    
+    frame_list = dir(sequence_dir);
+    frame_list = frame_list(3: end);
+    for frame_ele = frame_list
+        tic;
+        frame_name = frame_ele.name;
+        disp(['generating rps for frame ', data_list(idx).name, ', ', frame_name]);
+        frame_path = fullfile(sequence_dir, frame_name);
+        
+        [~, rp_name, ~] = fileparts(frame_name);
+        rp_name = [rp_name, '.mat'];
+        rp_path = fullfile(rp_sequence_dir, rp_name);
+        
+        frame = imread(frame_path);
+        [candidates_mcg, ~] = im2mcg(frame, 'accurate', 1);
+        region_proposal = candidates_mcg.masks;
+        save(rp_path, 'region_proposal');
+        consumed_time = toc;
+        disp(['consumed_time: ', consumed_time])
+    end
+end
+           
+        
